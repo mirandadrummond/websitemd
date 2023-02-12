3ff4306a55d500940e1c1e3e4a6b49b8956e7dd5
mirandadrummond <mdrummond.ieu2021@student.ie.edu>
2023-02-12 14:37:31 +0100

    clean up of files

--- README.md
+++
@@ -1,7 +0,0 @@
-# websitemd
-
-Steps to commit:
-
-git add * 
-git commit -m 'note of changes'
-git push origin main / git push / git push origin ...
--- websitemd/src/pages/projects.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Projects = () => {
-return (
-	<h1>Here are some cool projects I've done </h1>
-);
-};
-
-export default Projects;
